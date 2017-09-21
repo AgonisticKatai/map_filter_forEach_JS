af4b9910aafd8237eef4346215099dcf10fe0109
Nacho Torrella <nachotorrella@gmail.com>
2017-09-21 20:46:02 +0200

    added exercises

---
+++ travessing_array_with_for.js
@@ -0,0 +1,10 @@
+// Print all the names in an array
+
+function print () {
+  var names = ['Ben', 'Jafar', 'Matt', 'Priya', 'Brian'], counter
+
+  for (counter = 0; counter < names.length; counter++) {
+    console.log(names[counter])
+  }
+}
+print()
---
+++ travessing_array_with_forEch.js
@@ -0,0 +1,9 @@
+// Use forEach to print all the names in an array
+
+function print () {
+  var names = ['Ben', 'Jafar', 'Matt', 'Priya', 'Brian']
+  names.forEach(function (name) {
+    console.log(name)
+  })
+}
+print()
